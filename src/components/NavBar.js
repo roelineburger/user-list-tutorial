8642cae0b5ac904d44c3f5cc8bc48d753764f253
roelineburger <93773777+roelineburger@users.noreply.github.com>
2021-11-07 21:50:58 +0100

    Connect book data to booklist

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,13 @@ import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    /*<div>
-      <Link to="/">Home</Link>&nbsp;
-      <Link to="/books">Books</Link>
-    </div>*/
-    <nav class="navbar navbar-light bg-light navbar-expand-lg mb-4">
-      <div class="container">
+    <nav className="navbar navbar-light bg-light navbar-expand-lg mb-4">
+      <div className="container">
         <Link className="navbar-brand" to="/">
           Bookstore inc.
         </Link>
-        <div class="navbar-collapse">
-          <div class="navbar-nav">
+        <div className="navbar-collapse">
+          <div className="navbar-nav">
             <NavLink className="nav-link" to="/">
               Home
             </NavLink>
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,7 +1,33 @@
 import NavBar from "../components/NavBar";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function BookList() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const responsePromise = fetch("http://localhost:4000/books");
+    responsePromise.then((response) => {
+      const dataPromise = response.json();
+      dataPromise.then((data) => {
+        setBooks(data);
+      });
+    });
+  }, []);
+
+  const rows = books.map((book) => {
+    return (
+      <tr key={book.id}>
+        <td>{book.id}</td>
+        <td>
+          <Link to={`/books/${book.id}`}>{book.title}</Link>
+        </td>
+        <td>{book.author}</td>
+        <td>{book.yearPublished}</td>
+      </tr>
+    );
+  });
+
   return (
     <div>
       <NavBar />
@@ -9,7 +35,7 @@ function BookList() {
         <h3>
           Books <button className="btn btn-primary btn-sm">Add book</button>
         </h3>
-        <table class="table">
+        <table className="table">
           <thead>
             <tr>
               <th scope="col">ID</th>
@@ -18,16 +44,7 @@ function BookList() {
               <th scope="col">Publish date</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td>123</td>
-              <td>
-                <Link to="/books/123">Daring greatly</Link>
-              </td>
-              <td>Brene Brown</td>
-              <td>2017</td>
-            </tr>
-          </tbody>
+          <tbody>{rows}</tbody>
         </table>
       </div>
     </div>
